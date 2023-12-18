50ab88f2924189e7ae5a86e4dd290dbf419a6f94
FabricatoriusMachinator <mafl96@hotmail.com>
2023-12-18 14:20:19 +0100

    Created test demo
    Dennis og Ole Emil legger til logikk

---
+++ test.py
@@ -0,0 +1,7 @@
+def adddition(num1, num2):
+    ## Ole Emil: Legg til logik for legge sammen to tall
+    return 0
+
+def subtraction(num1, num2):
+    ## Dennis: Legg til logik for trekke fra
+    return 0
