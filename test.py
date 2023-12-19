b9039cb730a5e021ba9d6bd47b8eae7f1c88278a
Ole Emil Øygarden <oleemil89@gmail.com>
2023-12-19 09:16:53 +0100

    test with addition
    la til logik for å legge samm

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def adddition(num1, num2):
     ## Ole Emil: Legg til logik for legge sammen to tall
-    return 0
+    return num1+num2
 
 def subtraction(num1, num2):
     ## Dennis: Legg til logik for trekke fra
