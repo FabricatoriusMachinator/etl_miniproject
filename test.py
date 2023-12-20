e803bd4b53d17ac8bc2d367f4c0c848f620be68e
mag_flo <56078778+FabricatoriusMachinator@users.noreply.github.com>
2023-12-20 17:45:00 +0100

    Flights are gold

--- test.py
+++
@@ -1,7 +0,0 @@
-def adddition(num1, num2):
-    ## Ole Emil: Legg til logik for legge sammen to tall
-    return num1+num2
-
-def subtraction(num1, num2):
-    ## Dennis: Legg til logik for trekke fra
-    return 0
